345f6a996fabb2ac4f0ea47cae3b048eac2c9384
motherwort <32970725+motherwort@users.noreply.github.com>
2024-02-19 10:44:32 +0300

    feat: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 ssh root@myremote rm -rf ./plato
-scp -r . root@myremote:~/plato
+ssh root@myremote mkdir ./plato/
+ssh root@myremote mkdir ./plato/docker/
+ssh root@myremote mkdir ./plato/src/
+scp -r ./docker root@myremote:~/plato
+scp -r ./src root@myremote:~/plato
+scp -r ./poetry.lock root@myremote:~/plato
+scp -r ./pyproject.toml root@myremote:~/plato
 ssh root@myremote docker compose -f ./plato/docker/docker-compose.yml --project-directory ./plato up -d --build
